1f4de8a1d164129bdf755c26e36b90ac1565f1e9
aslanliIbrahim <ibrahimra@code.edu.az>
2025-02-17 17:01:41 +0400

    🚀 feat: scrool add

--- digit-landing-page/src/App.jsx
+++ digit-landing-page/src/App.jsx
@@ -1,10 +1,34 @@
+import "lenis/dist/lenis.css";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Header from "./section/Header";
+import Lenis from "lenis";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+      direction: "vertical", 
+      smooth: true, 
+      smoothTouch: false, 
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
   return (
-    <div className="">
+    <div>
       <Header />
       <Home />
       <Footer />
